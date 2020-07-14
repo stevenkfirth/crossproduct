0157135281c41ccb02cbdae4a73de5516cb1661b
Steven Firth <s.k.firth@lboro.ac.uk>
2020-07-14 07:54:18 +0100

    Updates

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,8 @@
+# added 14-7-2020
+master_doc = 'index'
+
+
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
