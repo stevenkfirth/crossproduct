2b639f3f075da1d4be704b21dd3887a0179039af
Steven Firth <s.k.firth@lboro.ac.uk>
2020-07-14 08:19:17 +0100

    Updates

--- docs/conf.py
+++ docs/conf.py
@@ -15,10 +15,10 @@ master_doc = 'index'
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+#print(sys)
 
 # -- Project information -----------------------------------------------------
 
@@ -33,6 +33,7 @@ author = 'Steven K Firth'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/index.rst
+++ docs/index.rst
@@ -12,6 +12,8 @@ Geometry in Python - 2D and 3D
    :maxdepth: 2
    :caption: Contents:
 
+.. automodule:: crossproduct.point
+    :members:
 
 
 Indices and tables
